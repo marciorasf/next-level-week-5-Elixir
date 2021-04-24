cabf8c2d961a0cd8b51d04066ba1fc477c0f5c4a
marciorasf <marciorasf@gmail.com>
2021-04-24 10:32:38 -0300

    fix typo

--- lib/inmana/supplies/get_by_expiration.ex
+++ lib/inmana/supplies/get_by_expiration.ex
@@ -1,7 +1,7 @@
 defmodule Inmana.Supplies.GetByExpiration do
   import Ecto.Query
 
-  alias Inmana.{Repo, Restauramt, Supply}
+  alias Inmana.{Repo, Restaurant, Supply}
 
   def call do
     today = Date.utc_today()
@@ -13,5 +13,9 @@ defmodule Inmana.Supplies.GetByExpiration do
         where:
           supply.expiration_date >= ^beginning_of_week and supply.expiration_date <= ^end_of_week,
         preload: [:restaurant]
+
+    query
+    |> Repo.all()
+    |> Enum.group_by(fn %Supply{restaurant: %Restaurant{email: email}} -> email end)
   end
 end
